caa26fae3e5110f38fb33e7ee20fd0f7eab42e75
jmapps <jmappsmuslim@gmail.com>
2020-10-21 12:15:43 -0700

    Added task category list

--- app/src/main/java/jmapps/addmyfavoriteword/presentation/ui/fragments/TasksFragment.kt
+++ app/src/main/java/jmapps/addmyfavoriteword/presentation/ui/fragments/TasksFragment.kt
@@ -1,37 +1,61 @@
-package jmapps.addmyfavoriteword.presentation.ui.tasks
+package jmapps.addmyfavoriteword.presentation.ui.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import jmapps.addmyfavoriteword.R
 import jmapps.addmyfavoriteword.databinding.FragmentTasksBinding
 import jmapps.addmyfavoriteword.presentation.mvp.otherFragments.ContractInterface
 import jmapps.addmyfavoriteword.presentation.mvp.otherFragments.OtherFragmentsPresenter
+import jmapps.addmyfavoriteword.presentation.ui.adapter.TaskCategoriesAdapter
+import jmapps.addmyfavoriteword.presentation.ui.bottomsheets.AddTaskCategory
+import jmapps.addmyfavoriteword.presentation.ui.model.TasksViewModel
 
-class TasksFragment : Fragment(), ContractInterface.OtherView {
+class TasksFragment : Fragment(), ContractInterface.OtherView,
+    TaskCategoriesAdapter.OnItemClickTaskCategory, View.OnClickListener {
 
     private lateinit var tasksViewModel: TasksViewModel
     private lateinit var binding: FragmentTasksBinding
     private lateinit var otherFragmentsPresenter: OtherFragmentsPresenter
+
+    private lateinit var taskCategoriesAdapter: TaskCategoriesAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         tasksViewModel = ViewModelProvider(this).get(TasksViewModel::class.java)
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_tasks, container, false)
 
         otherFragmentsPresenter = OtherFragmentsPresenter(this)
+        otherFragmentsPresenter.initView()
         otherFragmentsPresenter.defaultState()
-        //otherPresenter.updateState(testList)
 
-        tasksViewModel.text.observe(viewLifecycleOwner, Observer {
-        })
+        binding.fabAddTaskCategory.setOnClickListener(this)
 
         return binding.root
+    }
+
+    override fun onClick(v: View?) {
+        val addTaskCategory = AddTaskCategory()
+        addTaskCategory.show(childFragmentManager, "tagg")
+    }
+
+    override fun initView() {
+        tasksViewModel.allTaskCategories.observe(this, Observer { taskCategoriesList ->
+            taskCategoriesList.let {
+                taskCategoriesAdapter = TaskCategoriesAdapter(requireContext(), taskCategoriesList, this)
+                val verticalLayout = LinearLayoutManager(requireContext())
+                binding.rvTaskCategories.layoutManager = verticalLayout
+                binding.rvTaskCategories.adapter = taskCategoriesAdapter
+                otherFragmentsPresenter.updateState(taskCategoriesList)
+            }
+        })
     }
 
     override fun defaultState() {
@@ -45,5 +69,9 @@ class TasksFragment : Fragment(), ContractInterface.OtherView {
     override fun updateState() {
         binding.rvTaskCategories.visibility = otherFragmentsPresenter.recyclerCategory()
         binding.textMainViewDescription.visibility = otherFragmentsPresenter.descriptionMain()
+    }
+
+    override fun onItemClickTaskCategory(_id: Long) {
+        Toast.makeText(requireContext(), "Id = $_id", Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/jmapps/addmyfavoriteword/presentation/ui/model/TasksViewModel.kt
@@ -0,0 +1,33 @@
+package jmapps.addmyfavoriteword.presentation.ui.model
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import jmapps.addmyfavoriteword.data.database.room.tasks.TaskCategories
+import jmapps.addmyfavoriteword.data.database.room.tasks.TaskDatabaseHelper
+import jmapps.addmyfavoriteword.presentation.ui.repository.TaskCategoriesRepository
+import kotlinx.coroutines.launch
+
+class TasksViewModel(application: Application) : AndroidViewModel(application) {
+    private val taskCategoryRepository: TaskCategoriesRepository
+    val allTaskCategories: LiveData<MutableList<TaskCategories>>
+
+    init {
+        val taskCategoriesDao = TaskDatabaseHelper.getDatabase(application, viewModelScope).taskCategoriesDao()
+        taskCategoryRepository = TaskCategoriesRepository(taskCategoriesDao)
+        allTaskCategories = taskCategoryRepository.allTaskCategories
+    }
+
+    fun insertTaskCategory(taskCategories: TaskCategories) = viewModelScope.launch {
+        taskCategoryRepository.insertTaskCategory(taskCategories)
+    }
+
+    fun updateTaskCategory(newTitle: String) = viewModelScope.launch {
+        taskCategoryRepository.updateTaskCategory(newTitle)
+    }
+
+    fun deleteTaskCategory(_id: Long) = viewModelScope.launch {
+        taskCategoryRepository.deleteTaskCategory(_id)
+    }
+}
