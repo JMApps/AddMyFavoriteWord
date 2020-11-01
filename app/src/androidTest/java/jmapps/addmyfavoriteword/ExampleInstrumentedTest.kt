5a40689398a7fa85f6f54942d6202de2d3c9bee5
jmapps <jmappsmuslim@gmail.com>
2020-11-01 09:22:34 -0800

    Fixes items

--- app/src/androidTest/java/jmapps/addmyfavoriteword/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/jmapps/addmyfavoriteword/ExampleInstrumentedTest.kt
@@ -2,6 +2,8 @@ package jmapps.addmyfavoriteword
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import jmapps.addmyfavoriteword.presentation.mvp.otherFragments.ContractInterface
+import jmapps.addmyfavoriteword.presentation.mvp.otherFragments.OtherFragmentsPresenter
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -14,11 +16,36 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class ExampleInstrumentedTest : ContractInterface.OtherView {
+    private lateinit var otherFragmentsPresenter: OtherFragmentsPresenter
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("jmapps.addmyfavoriteword", appContext.packageName)
+    }
+
+    @Test
+    fun useOtherMvp() {
+        val list = mutableListOf<Any>(1, 2, 3)
+
+        otherFragmentsPresenter = OtherFragmentsPresenter(this)
+        otherFragmentsPresenter.defaultState()
+        otherFragmentsPresenter.updateState(list)
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun defaultState() {
+        otherFragmentsPresenter.recyclerCategory()
+        otherFragmentsPresenter.descriptionMain()
+    }
+
+    override fun updateState() {
+        otherFragmentsPresenter.recyclerCategory()
+        otherFragmentsPresenter.descriptionMain()
     }
 }
