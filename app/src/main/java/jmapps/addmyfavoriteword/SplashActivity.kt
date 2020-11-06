d80a8ac0c2ab558763d7f964a32adb65f45e893e
jmapps <jmappsmuslim@gmail.com>
2020-11-06 14:56:41 -0800

    Added splash screen

---
+++ app/src/main/java/jmapps/addmyfavoriteword/SplashActivity.kt
@@ -0,0 +1,14 @@
+package jmapps.addmyfavoriteword
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+
+class SplashActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val toMainActivity = Intent(this, MainActivity::class.java)
+        startActivity(toMainActivity)
+    }
+}
