85c7e44f0eb460d9e763349de6beaa3df3e703b8
jmapps <jmappsmuslim@gmail.com>
2020-12-05 09:05:49 -0800

    Added logic change note

--- app/src/main/java/jmapps/addmyfavoriteword/presentation/ui/activities/ChangeNoteActivity.kt
+++ app/src/main/java/jmapps/addmyfavoriteword/presentation/ui/activities/ChangeNoteActivity.kt
@@ -35,13 +35,18 @@ class ChangeNoteActivity : AppCompatActivity(), QuestionAlertUtil.OnClickQuestio
     private var currentNoteTitle: String? = null
     private var currentNoteContent: String? = null
 
+    private var newNoteColor: String? = null
+    private var newNotePriority: Long? = null
+    private var newNoteTitle: String? = null
+    private var newNoteContent: String? = null
+
     companion object {
         private const val KEY_RESTORE_CURRENT_NOTE_COLOR = "key_restore_current_note_color"
-        private const val KEY_CURRENT_NOTE_ID = "key_current_note_id"
-        private const val KEY_CURRENT_NOTE_COLOR = "key_current_note_color"
-        private const val KEY_CURRENT_NOTE_PRIORITY = "key_current_note_priority"
-        private const val KEY_CURRENT_NOTE_TITLE = "key_current_note_title"
-        private const val KEY_CURRENT_NOTE_CONTENT = "key_current_note_content"
+        const val KEY_CURRENT_NOTE_ID = "key_current_note_id"
+        const val KEY_CURRENT_NOTE_COLOR = "key_current_note_color"
+        const val KEY_CURRENT_NOTE_PRIORITY = "key_current_note_priority"
+        const val KEY_CURRENT_NOTE_TITLE = "key_current_note_title"
+        const val KEY_CURRENT_NOTE_CONTENT = "key_current_note_content"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,6 +56,7 @@ class ChangeNoteActivity : AppCompatActivity(), QuestionAlertUtil.OnClickQuestio
         setSupportActionBar(binding.toolbar)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        questionAlertUtil = QuestionAlertUtil(this, this)
 
         currentNoteId = intent.getLongExtra(KEY_CURRENT_NOTE_ID, 0)
         currentNoteColor = intent.getStringExtra(KEY_CURRENT_NOTE_COLOR)
@@ -58,12 +64,19 @@ class ChangeNoteActivity : AppCompatActivity(), QuestionAlertUtil.OnClickQuestio
         currentNoteTitle = intent.getStringExtra(KEY_CURRENT_NOTE_TITLE)
         currentNoteContent = intent.getStringExtra(KEY_CURRENT_NOTE_CONTENT)
 
-        DrawableCompat.setTint(binding.changeNoteItemContent.textChangeNoteColor.background, Color.parseColor(currentNoteColor))
+        newNoteColor = currentNoteColor
+        newNotePriority = currentNotePriority
+        newNoteTitle = currentNoteTitle
+        newNoteContent = currentNoteContent
 
-        questionAlertUtil = QuestionAlertUtil(this, this)
-
-        val noteTitleCharacters = getString(R.string.max_note_title_characters, 0)
-        binding.changeNoteItemContent.textLengthChangeNoteCharacters.text = noteTitleCharacters
+        binding.apply {
+            DrawableCompat.setTint(changeNoteItemContent.textChangeNoteColor.background, Color.parseColor(currentNoteColor))
+            changeNoteItemContent.spinnerNoteNewPriority.setSelection(currentNotePriority!!.toInt())
+            changeNoteItemContent.editChangeNoteItemTitle.setText(currentNoteTitle)
+            val currentNoteTitleCharacters = getString(R.string.max_note_title_characters, currentNoteTitle?.length)
+            changeNoteItemContent.textLengthChangeNoteCharacters.text = currentNoteTitleCharacters
+            changeNoteItemContent.editChangeNoteItemContent.setText(currentNoteContent)
+        }
 
         binding.changeNoteItemContent.textChangeNoteColor.setOnClickListener(this)
         binding.changeNoteItemContent.editChangeNoteItemTitle.addTextChangedListener(this)
@@ -71,18 +84,23 @@ class ChangeNoteActivity : AppCompatActivity(), QuestionAlertUtil.OnClickQuestio
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_add_note, menu)
-        itemChangeNote = menu!!.findItem(R.id.action_add_note)
-        itemChangeNote?.isVisible = checkEditTexts()
+        menuInflater.inflate(R.menu.menu_change_note, menu)
+        itemChangeNote = menu!!.findItem(R.id.action_change_note)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onBackPressed() {
-        if (checkEditTexts()) {
-            if (checkInitialNewValues()) {
-                questionAlertUtil.showAlertDialog(getString(R.string.dialog_message_are_sure_you_want_change_item_note_without_save))
+        // Если поступившие данные изменились
+        if (checkAllData()) {
+            // Если поля ввода не пустые
+            if (checkEditTexts()) {
+                // Спросить, применить ли изменения перед выходом
+                questionAlertUtil.showAlertDialog(getString(R.string.dialog_message_are_sure_you_want_change_item_note_without_save),
+                    getString(R.string.alert_apply), getString(R.string.alert_not_apply))
             } else {
-                changeNote()
+                // Если поля ввода пустые закрыть окно вывести сообщение "действие отменено"
+                Toast.makeText(this, getString(R.string.action_canceled), Toast.LENGTH_SHORT).show()
+                super.onBackPressed()
             }
         } else {
             super.onBackPressed()
@@ -103,20 +121,37 @@ class ChangeNoteActivity : AppCompatActivity(), QuestionAlertUtil.OnClickQuestio
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
-                if (checkEditTexts()) {
-                    if (checkInitialNewValues()) {
-                        questionAlertUtil.showAlertDialog(getString(R.string.dialog_message_are_sure_you_want_change_item_note_without_save))
+                // Применить новые данные если они были изменены
+                applyIfChange()
+                // Если данные изменились
+                if (checkAllData()) {
+                    // Если поля ввода не пустые
+                    if (checkEditTexts()) {
+                        // Спросить, применить ли изменения перед выходом
+                        questionAlertUtil.showAlertDialog(getString(R.string.dialog_message_are_sure_you_want_change_item_note_without_save),
+                            getString(R.string.alert_apply), getString(R.string.alert_not_apply))
                     } else {
-                        changeNote()
+                        // Если поля ввода пустые закрыть окно вывести сообщение "действие отменено"
+                        Toast.makeText(this, getString(R.string.action_canceled), Toast.LENGTH_SHORT).show()
+                        finish()
                     }
                 } else {
-                    finish()
+                    changeNote()
                 }
             }
             R.id.action_change_note -> {
-                if (checkEditTexts()) {
-                    assignNewValues()
-                    clearFocus()
+                // Применить новые данные если они были изменены
+                applyIfChange()
+                // Если данные изменились
+                if (checkAllData()) {
+                    // Если поля ввода не пустые
+                    if (checkEditTexts()) {
+                        assignNewValues()
+                        clearFocus()
+                    } else {
+                        Toast.makeText(this, getString(R.string.action_canceled), Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
                 } else {
                     Toast.makeText(this, getString(R.string.action_canceled), Toast.LENGTH_SHORT).show()
                     finish()
@@ -151,7 +186,8 @@ class ChangeNoteActivity : AppCompatActivity(), QuestionAlertUtil.OnClickQuestio
                 Toast.makeText(this, getString(R.string.toast_achieved_max_note_title_characters), Toast.LENGTH_SHORT).show()
             }
         }
-        itemChangeNote?.isVisible = checkEditTexts()
+        applyIfChange()
+        itemChangeNote?.isVisible = checkAllData()
     }
 
     override fun afterTextChanged(s: Editable?) {}
@@ -162,8 +198,27 @@ class ChangeNoteActivity : AppCompatActivity(), QuestionAlertUtil.OnClickQuestio
     }
 
     override fun onClickDelete() {
-        Toast.makeText(this, getString(R.string.toast_deleted), Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, getString(R.string.action_canceled), Toast.LENGTH_SHORT).show()
         finish()
+    }
+
+    private fun applyIfChange() {
+        when {
+            currentNotePriority != binding.changeNoteItemContent.spinnerNoteNewPriority.selectedItemId -> {
+                currentNotePriority = binding.changeNoteItemContent.spinnerNoteNewPriority.selectedItemId
+            }
+            currentNoteTitle != binding.changeNoteItemContent.editChangeNoteItemTitle.text.toString() -> {
+                currentNoteTitle = binding.changeNoteItemContent.editChangeNoteItemTitle.text.toString()
+            }
+            currentNoteContent != binding.changeNoteItemContent.editChangeNoteItemContent.text.toString() -> {
+                currentNoteContent = binding.changeNoteItemContent.editChangeNoteItemContent.text.toString()
+            }
+        }
+    }
+
+    private fun checkAllData(): Boolean {
+        return newNoteColor != currentNoteColor || newNotePriority != currentNotePriority ||
+                newNoteTitle != currentNoteTitle || newNoteContent != currentNoteContent
     }
 
     private fun checkEditTexts(): Boolean {
@@ -171,14 +226,11 @@ class ChangeNoteActivity : AppCompatActivity(), QuestionAlertUtil.OnClickQuestio
                 binding.changeNoteItemContent.editChangeNoteItemContent.text.toString().isNotEmpty()
     }
 
-    private fun checkInitialNewValues(): Boolean {
-        return currentNoteTitle != binding.changeNoteItemContent.editChangeNoteItemTitle.text.toString() ||
-                currentNoteContent != binding.changeNoteItemContent.editChangeNoteItemContent.text.toString()
-    }
-
     private fun assignNewValues () {
-        currentNoteTitle = binding.changeNoteItemContent.editChangeNoteItemTitle.text.toString()
-        currentNoteContent = binding.changeNoteItemContent.editChangeNoteItemContent.text.toString()
+        newNoteColor = currentNoteColor
+        newNotePriority = currentNotePriority
+        newNoteTitle = currentNoteTitle
+        newNoteContent = currentNoteContent
         itemChangeNote?.isVisible = false
     }
 
@@ -189,7 +241,7 @@ class ChangeNoteActivity : AppCompatActivity(), QuestionAlertUtil.OnClickQuestio
     }
 
     private fun closeKeyboard() {
-        binding.changeNoteItemContent.editChangeNoteItemContent.postDelayed(Runnable {
+        binding.changeNoteItemContent.editChangeNoteItemContent.postDelayed({
             val keyboard: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             keyboard.hideSoftInputFromWindow(binding.changeNoteItemContent.editChangeNoteItemContent.windowToken, 0)
         }, 200)
@@ -197,10 +249,10 @@ class ChangeNoteActivity : AppCompatActivity(), QuestionAlertUtil.OnClickQuestio
 
     private fun changeNote() {
         notesItemViewModel.updateNoteItem(
-            currentNoteTitle!!,
-            currentNoteContent!!,
-            currentNoteColor!!,
-            currentNotePriority!!,
+            newNoteTitle!!,
+            newNoteContent!!,
+            newNoteColor!!,
+            newNotePriority!!,
             currentNoteId!!
         )
         Toast.makeText(this, getString(R.string.toast_note_changed), Toast.LENGTH_SHORT).show()
