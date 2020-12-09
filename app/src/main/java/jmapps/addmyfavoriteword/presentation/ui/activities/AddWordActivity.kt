97766f7fb8002c6bcfe1fd91908bdbdbaf5b57a4
jmapps <jmappsmuslim@gmail.com>
2020-12-09 08:57:13 -0800

    Fixed add word with little change's

--- app/src/main/java/jmapps/addmyfavoriteword/presentation/ui/activities/AddWordActivity.kt
+++ app/src/main/java/jmapps/addmyfavoriteword/presentation/ui/activities/AddWordActivity.kt
@@ -1,20 +1,46 @@
 package jmapps.addmyfavoriteword.presentation.ui.activities
 
+import android.app.SearchManager
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.Html
+import android.text.TextUtils
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.databinding.DataBindingUtil
+import androidx.preference.PreferenceManager
+import androidx.recyclerview.widget.RecyclerView
 import jmapps.addmyfavoriteword.R
 import jmapps.addmyfavoriteword.databinding.ActivityAddWordBinding
+import jmapps.addmyfavoriteword.presentation.mvp.otherActivities.ContractInterface
+import jmapps.addmyfavoriteword.presentation.mvp.otherActivities.OtherActivityPresenter
+import jmapps.addmyfavoriteword.presentation.ui.models.WordsItemViewModel
+import jmapps.addmyfavoriteword.presentation.ui.other.DeleteAlertUtil
+import jmapps.addmyfavoriteword.presentation.ui.preferences.SharedLocalProperties
 
-class AddWordActivity : AppCompatActivity() {
+class AddWordActivity : AppCompatActivity(), ContractInterface.OtherView,
+    DeleteAlertUtil.OnClickDelete, View.OnClickListener, SearchView.OnQueryTextListener {
 
     private lateinit var binding: ActivityAddWordBinding
+    private lateinit var wordsItemViewModel: WordsItemViewModel
+
+    private lateinit var preferences: SharedPreferences
+    private lateinit var sharedLocalPreferences: SharedLocalProperties
+    private lateinit var otherActivityPresenter: OtherActivityPresenter
 
     private var wordCategoryId: Long? = null
     private var wordCategoryTitle: String? = null
     private var wordCategoryColor: String? = null
     private var wordCategoryPriority: Long? = null
     private var defaultOrderIndex: Int? = null
+
+    private lateinit var searchView: SearchView
+    private lateinit var wordItemsAdapter: WordItemsAdapter
+
+    private lateinit var deleteAlertDialog: DeleteAlertUtil
 
     companion object {
         const val KEY_WORD_CATEGORY_ID = "key_word_category_id"
@@ -40,5 +66,119 @@ class AddWordActivity : AppCompatActivity() {
             it.setDisplayHomeAsUpEnabled(true)
             it.title = wordCategoryTitle
         }
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        sharedLocalPreferences = SharedLocalProperties(preferences)
+        defaultOrderIndex = sharedLocalPreferences.getIntValue(KEY_ORDER_WORD_INDEX, 0)!!
+
+        otherActivityPresenter = OtherActivityPresenter(this)
+        otherActivityPresenter.initView(wordCategoryId!!, defaultOrderIndex!!)
+
+        deleteAlertDialog = DeleteAlertUtil(this, this)
+
+        binding.addWordItemContent.rvWordItems.addOnScrollListener(onAddScroll)
+        binding.addWordItemContent.fabAddWordItem.setOnClickListener(this)
+    }
+
+    override fun initView(displayBy: Long, orderBy: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun defaultState() {
+        binding.addWordItemContent.rvWordItems.visibility = otherActivityPresenter.recyclerCategory()
+        binding.addWordItemContent.textMainWordDescription.visibility = otherActivityPresenter.descriptionMain()
+        val categoryDescription = getString(R.string.description_add_first_word, "<b>$wordCategoryTitle</b>")
+        binding.addWordItemContent.textMainWordDescription.text = Html.fromHtml(categoryDescription)
+    }
+
+    override fun updateState() {
+        binding.addWordItemContent.rvWordItems.visibility = otherActivityPresenter.recyclerCategory()
+        binding.addWordItemContent.textMainWordDescription.visibility = otherActivityPresenter.descriptionMain()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_word_items, menu)
+        val searchManager = getSystemService(SEARCH_SERVICE) as SearchManager
+        searchView = menu.findItem(R.id.action_search_words).actionView as SearchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.maxWidth = Integer.MAX_VALUE
+        searchView.setOnQueryTextListener(this)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> finish()
+
+            R.id.action_tools_word_items -> {
+
+            }
+            R.id.item_order_by_add_time -> {
+                changeOrderList(defaultOrderIndex = 0)
+            }
+            R.id.item_order_by_execution -> {
+                changeOrderList(defaultOrderIndex = 1)
+            }
+            R.id.item_order_by_alphabet -> {
+                changeOrderList(defaultOrderIndex = 4)
+            }
+            R.id.action_delete_all_word_items -> {
+                val deleteAllWordDescription = getString(R.string.dialog_message_are_sure_you_want_items_word, "<b>$wordCategoryTitle</b>")
+                deleteAlertDialog.showAlertDialog(deleteAllWordDescription, 0, 0, getString(R.string.action_words_deleted))
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        return false
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        if (TextUtils.isEmpty(newText)) {
+            wordItemsAdapter.filter.filter("")
+        } else {
+            wordItemsAdapter.filter.filter(newText)
+        }
+        return true
+    }
+
+    override fun onClick(v: View?) {
+        val addWordItem = AddWordItemBottomSheet.toInstance(wordCategoryId!!, wordCategoryColor!!, wordCategoryPriority!!)
+        addWordItem.show(supportFragmentManager, AddWordItemBottomSheet.ARG_ADD_WORD_ITEM_BS)
+    }
+
+    override fun itemClickRenameItem(wordItemId: Long, word: String, wordTranscription: String, wordTranslate: String) {
+        val renameWordItem = RenameWordItemBottomSheet.toInstance(wordItemId, word, wordTranscription, wordTranslate)
+        renameWordItem.show(supportFragmentManager, RenameWordItemBottomSheet.ARG_RENAME_WORD_ITEM_BS)
+    }
+
+    override fun itemClickDeleteItem(wordItemId: Long) {
+        deleteAlertDialog.showAlertDialog(getString(R.string.dialog_message_are_sure_you_want_item_word), 1, wordItemId, getString(R.string.action_word_deleted))
+    }
+
+    override fun onClickDeleteOnly(_id: Long) {
+        wordsItemViewModel.deleteWordItem(_id)
+    }
+
+    override fun onClickDeleteAll() {
+        wordsItemViewModel.deleteAllWordFromCategory(wordCategoryId!!)
+    }
+
+    private val onAddScroll = object : RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            if (dy > 0) {
+                binding.addWordItemContent.fabAddWordItem.hide()
+            } else {
+                binding.addWordItemContent.fabAddWordItem.show()
+            }
+        }
+    }
+
+    private fun changeOrderList(defaultOrderIndex: Int) {
+        sharedLocalPreferences.saveIntValue(KEY_ORDER_WORD_INDEX, defaultOrderIndex)
+        otherActivityPresenter.initView(wordCategoryId!!, defaultOrderIndex)
+        recreate()
     }
 }
---
+++ app/src/main/java/jmapps/addmyfavoriteword/presentation/ui/holders/WordItemsHolder.kt
@@ -0,0 +1,8 @@
+package jmapps.addmyfavoriteword.presentation.ui.holders
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class WordItemsHolder(wordView: View) : RecyclerView.ViewHolder(wordView) {
+
+}
