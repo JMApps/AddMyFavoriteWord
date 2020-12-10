2823c8a5098f44fae8e4826d9c56e9a58f292b6a
jmapps <jmappsmuslim@gmail.com>
2020-12-09 19:07:56 -0800

    Added word items adapter and fixes word holder

---
+++ app/src/main/java/jmapps/addmyfavoriteword/presentation/ui/adapters/WordItemsAdapter.kt
@@ -0,0 +1,93 @@
+package jmapps.addmyfavoriteword.presentation.ui.adapters
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
+import androidx.core.graphics.drawable.DrawableCompat
+import androidx.recyclerview.widget.RecyclerView
+import jmapps.addmyfavoriteword.R
+import jmapps.addmyfavoriteword.data.database.room.dictionary.words.WordItems
+import jmapps.addmyfavoriteword.presentation.ui.holders.WordItemsHolder
+
+class WordItemsAdapter(
+    private val context: Context,
+    private var wordItemList: MutableList<WordItems>,
+    private val onLongWordItemClick: OnLongWordItemClick) : RecyclerView.Adapter<WordItemsHolder>(), Filterable {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+    private var firstWordItemList: MutableList<WordItems>? = null
+
+    init {
+        this.firstWordItemList = wordItemList
+    }
+
+    interface OnLongWordItemClick {
+        fun itemClickRenameItem(wordItemId: Long, word: String, wordTranscription: String, wordTranslate: String)
+
+        fun itemClickDeleteItem(wordItemId: Long)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordItemsHolder {
+        val viewItem = inflater.inflate(R.layout.item_word, parent, false)
+        return (WordItemsHolder(viewItem))
+    }
+
+    override fun onBindViewHolder(holder: WordItemsHolder, position: Int) {
+        val current = wordItemList[position]
+
+        holder.tvWordColor.setBackgroundColor(Color.parseColor(current.wordItemColor))
+
+        holder.tvWord.text = current.word
+        holder.tvWordTranscription.text = current.wordTranscription
+        holder.tvWordTranslate.text = current.wordTranslate
+
+        val priorityName = arrayListOf("#C5C5C5", "#F4FF81", "#00C853", "#FF3D00")
+        DrawableCompat.setTint(holder.tvWordPriority.background, Color.parseColor(priorityName[current.priority.toInt()]))
+        holder.tvWordPriority.text = (position + 1).toString()
+
+        val wordAddDateTime = context.getString(R.string.action_add_time_item_word, "\n${current.addDateTime}")
+        val wordChangeDateTime = context.getString(R.string.action_change_time_item_word, "\n${current.changeDateTime}")
+
+        holder.tvWordAddDateTime.text = wordAddDateTime
+        holder.tvWordChangeDateTime.text = wordChangeDateTime
+
+        holder.findOnLongWordItemClick(onLongWordItemClick, current._id, current.word, current.wordTranscription, current.wordTranslate)
+    }
+
+    override fun getItemCount() = wordItemList.size
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            @SuppressLint("DefaultLocale")
+            override fun performFiltering(charSequence: CharSequence): FilterResults {
+                val charString = charSequence.toString()
+                wordItemList = if (charString.isEmpty()) {
+                    firstWordItemList as MutableList<WordItems>
+                } else {
+                    val filteredList = ArrayList<WordItems>()
+                    for (row in firstWordItemList!!) {
+                        if (row._id.toString().contains(charSequence) ||
+                            row.word.toLowerCase().contains(charString.toLowerCase()) ||
+                            row.wordTranscription.toLowerCase().contains(charString.toLowerCase()) ||
+                            row.wordTranslate.toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(row)
+                        }
+                    }
+                    filteredList
+                }
+                val filterResults = FilterResults()
+                filterResults.values = wordItemList
+                return filterResults
+            }
+
+            override fun publishResults(charSequence: CharSequence, filterResults: FilterResults) {
+                wordItemList = filterResults.values as ArrayList<WordItems>
+                notifyDataSetChanged()
+            }
+        }
+    }
+}
--- app/src/main/java/jmapps/addmyfavoriteword/presentation/ui/holders/WordItemsHolder.kt
+++ app/src/main/java/jmapps/addmyfavoriteword/presentation/ui/holders/WordItemsHolder.kt
@@ -1,8 +1,82 @@
 package jmapps.addmyfavoriteword.presentation.ui.holders
 
+import android.content.SharedPreferences
 import android.view.View
+import android.widget.TextView
+import androidx.appcompat.widget.PopupMenu
+import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.RecyclerView
+import jmapps.addmyfavoriteword.R
+import jmapps.addmyfavoriteword.presentation.ui.adapters.WordItemsAdapter
+import jmapps.addmyfavoriteword.presentation.ui.preferences.SharedLocalProperties
 
-class WordItemsHolder(wordView: View) : RecyclerView.ViewHolder(wordView) {
+class WordItemsHolder(wordView: View) : RecyclerView.ViewHolder(wordView),
+    SharedPreferences.OnSharedPreferenceChangeListener {
+    private var preferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(itemView.context)
+    private var sharedLocalPreferences: SharedLocalProperties
 
+    val tvWordColor: TextView = wordView.findViewById(R.id.text_word_color)
+    val tvWord: TextView = wordView.findViewById(R.id.text_word)
+    val tvWordTranscription: TextView = wordView.findViewById(R.id.text_word_transcription)
+    val tvWordTranslate: TextView = wordView.findViewById(R.id.text_word_translate)
+    val tvWordPriority: TextView = wordView.findViewById(R.id.text_word_priority)
+    val tvWordAddDateTime: TextView = wordView.findViewById(R.id.text_word_add_date_time)
+    val tvWordChangeDateTime: TextView = wordView.findViewById(R.id.text_word_change_date_time)
+
+    init {
+        sharedLocalPreferences = SharedLocalProperties(preferences)
+        PreferenceManager.getDefaultSharedPreferences(itemView.context)
+            .registerOnSharedPreferenceChangeListener(this)
+//        setTextSize()
+//        setShowAddChangeDateTime()
+    }
+
+    fun findOnLongWordItemClick(onLongWordItemClick: WordItemsAdapter.OnLongWordItemClick, wordItemId: Long, word: String, wordTranscription: String, wordTranslate: String) {
+        itemView.setOnClickListener {
+            val pop = PopupMenu(itemView.context, tvWord)
+            pop.inflate(R.menu.menu_change_item_popup)
+            pop.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.popup_change_item -> {
+                        onLongWordItemClick.itemClickRenameItem(wordItemId, word, wordTranscription, wordTranslate)
+                    }
+
+                    R.id.popup_delete_item -> {
+                        onLongWordItemClick.itemClickDeleteItem(wordItemId)
+                    }
+                }
+                true
+            }
+            pop.show()
+        }
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+//        setTextSize()
+//        setShowAddChangeDateTime()
+    }
+
+//    private fun setTextSize() {
+//        val textSize = sharedLocalPreferences.getIntValue(KEY_WORD_ITEM_TEXT_SIZE, 18)
+//        tvWord.textSize = textSize!!.toFloat()
+//        tvWordTranscription.textSize = textSize.toFloat()
+//        tvWordTranslate.textSize = textSize.toFloat()
+//    }
+//
+//    private fun setShowAddChangeDateTime() {
+//        val addShowingDateTime = sharedLocalPreferences.getBooleanValue(KEY_WORD_ITEM_ADD_DATE_TIME, false)
+//        val changeShowingDateTime = sharedLocalPreferences.getBooleanValue(KEY_WORD_ITEM_CHANGE_DATE_TIME, false)
+//
+//        if (!addShowingDateTime!!) {
+//            tvWordAddDateTime.visibility = View.GONE
+//        } else {
+//            tvWordAddDateTime.visibility = View.VISIBLE
+//        }
+//
+//        if (!changeShowingDateTime!!) {
+//            tvWordChangeDateTime.visibility = View.GONE
+//        } else {
+//            tvWordChangeDateTime.visibility = View.VISIBLE
+//        }
+//    }
 }
